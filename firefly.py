cd0559eae60e82e72162d38813d6e9d2315fcaa2
Raymond Camden <raymondcamden@gmail.com>
2024-10-17 14:41:08 -0500

    initial

--- firefly.py
+++ firefly.py
@@ -1,11 +1,40 @@
+import requests 
+
 class FireflyServices:
 	
 	def __init__(self, clientId, clientSecret):
 		self.clientId = clientId
 		self.clientSecret = clientSecret
+		self.accessToken = ''
 
-	
+	def __getAccessToken(self):
+		if self.accessToken != '':
+			print('in cache')
+			return self.accessToken
 
-	def getFFAccessToken(id, secret):
-		#response = requests.post(f"https://ims-na1.adobelogin.com/ims/token/v3?client_id={id}&client_secret={secret}&grant_type=client_credentials&scope=openid,AdobeID,session,additional_info,read_organizations,firefly_api,ff_apis")
-		#return response.json()['access_token']
+		response = requests.post(f"https://ims-na1.adobelogin.com/ims/token/v3?client_id={self.clientId}&client_secret={self.clientSecret}&grant_type=client_credentials&scope=openid,AdobeID,session,additional_info,read_organizations,firefly_api,ff_apis")
+		self.accessToken = response.json()['access_token']
+		return self.accessToken
+
+	def textToImage(self, prompt, **kwargs):
+		token = self.__getAccessToken()
+
+		data = {
+			"prompt":prompt
+		}
+
+		data.update(kwargs)
+		#print(data)
+		
+		response = requests.post("https://firefly-api.adobe.io/v3/images/generate", json=data, headers = {
+			"X-API-Key":self.clientId, 
+			"Authorization":f"Bearer {token}",
+			"Content-Type":"application/json"
+		}) 
+
+		return response.json()
+
+	def download(self, url, path):
+		with open(path,'wb') as output:
+			bits = requests.get(url, stream=True).content
+			output.write(bits)
--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 from firefly import FireflyServices
+import os
 
 ff_client_id = os.environ.get('CLIENT_ID')
 ff_client_secret = os.environ.get('CLIENT_SECRET')
 
-
 ff = FireflyServices(ff_client_id, ff_client_secret)
+
+res = ff.textToImage("cat under sunset", numVariations=2)
+for output in res["outputs"]:
+	filename = f"output/{output['seed']}.jpg"
+	ff.download(output["image"]["url"], filename)
+	print(f"Saved {filename}")
+
