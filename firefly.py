7a3cbfddf3dbd1023ab03cd746cbd70a9b09d2fb
Raymond Camden <raymondcamden@gmail.com>
2024-10-17 13:23:11 -0500

    first

---
+++ firefly.py
@@ -0,0 +1,7 @@
+class FireflyServices:
+	
+	def __init__(self, clientId, clientSecret):
+		self.clientId = clientId
+		self.clientSecret = clientSecret
+
+	
---
+++ test.py
@@ -0,0 +1,3 @@
+from firefly import FireflyServices
+
+ff = FireflyServices(1,2)
