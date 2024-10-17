50b43c6a72c79cb88b3ae4f7918f415bb4715350
Raymond Camden <raymondcamden@gmail.com>
2024-10-17 13:30:45 -0500

    misc

--- firefly.py
+++ firefly.py
@@ -5,3 +5,7 @@ class FireflyServices:
 		self.clientSecret = clientSecret
 
 	
+
+	def getFFAccessToken(id, secret):
+		#response = requests.post(f"https://ims-na1.adobelogin.com/ims/token/v3?client_id={id}&client_secret={secret}&grant_type=client_credentials&scope=openid,AdobeID,session,additional_info,read_organizations,firefly_api,ff_apis")
+		#return response.json()['access_token']
--- test.py
+++ test.py
@@ -1,3 +1,7 @@
 from firefly import FireflyServices
 
-ff = FireflyServices(1,2)
+ff_client_id = os.environ.get('CLIENT_ID')
+ff_client_secret = os.environ.get('CLIENT_SECRET')
+
+
+ff = FireflyServices(ff_client_id, ff_client_secret)
